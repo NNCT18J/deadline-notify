3a09d4887e83b2eea4a97ababa597025bb6134e7
BonyChops <contact@bonychops.com>
2022-10-10 15:12:38 +0900

    Fix: add npm i

--- bb.js
+++ bb.js
@@ -3,21 +3,15 @@ const axios = require('axios');
 const ical = require('node-ical');
 
 (async () => {
-    try {
-        // or just parse some iCalendar data directly
-        const events = ical.sync.parseICS((await axios.get(process.env.BB_ICS_URL)));
+    // or just parse some iCalendar data directly
+    const events = ical.sync.parseICS((await axios.get(process.env.BB_ICS_URL)));
 
-        for (const event of Object.values(events)) {
-            console.log(
-                'Summary: ' + event.summary +
-                '\nDescription: ' + event.description +
-                '\nStart Date: ' + event.start.toISOString() +
-                '\n'
-            );
-        };
-    }catch(e){
-        console.error(e);
-        process.exit(1);
-    }
-
+    for (const event of Object.values(events)) {
+        console.log(
+            'Summary: ' + event.summary +
+            '\nDescription: ' + event.description +
+            '\nStart Date: ' + event.start.toISOString() +
+            '\n'
+        );
+    };
 })();
